d6de14811dc38b19d1ab9f08983e51e28d70d834
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2019-08-21 22:34:06 +0900

    Update DB Definition.

--- share/DB/20171120UpdateTables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE M_MODEL_PARAMETER (
-	model_id			integer not null,
-	model_param_id		integer not null,
-	name 				text not null,
-	value			text,
-	primary key(model_id, model_param_id)
-);
-
-vacuum;
